f42c8066ee8ace2012b2f2db2f2c0c5ca4043211
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-01-30 16:13:55 +0100

    Comment for solution without for or while loops

--- main.rb
+++ main.rb
@@ -26,3 +26,19 @@ end
 arr = [4,3,78,2,0,2]
 
 bubble_sort(arr)
+
+# a more elegant solution without using while or for loops:
+# def bubble_sort(arr)
+# (arr.length - 1).times do
+#   arr.each_with_index do |_, i|
+#     break if i == arr.length - 1
+#     if arr[i] > arr[i + 1]
+#       arr[i], arr[i + 1] = arr[i + 1], arr[i]  # Swap using parallel assignment
+#     end
+#   end
+# end
+# arr  # Return the sorted array
+# end
+
+# arr = [4,3,78,2,0,2]
+# p bubble_sort(arr)  # => [0,2,2,3,4,78]
